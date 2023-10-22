4036f97246dc740af0bfd1ced8bb7901d4cde999
mohamednasser <mohamed.nasser.sami.97@gmail.com>
2023-10-22 17:16:26 +0300

    create cart page

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { cartInfo } from "../../data/cart";
+import CartItem from "./CartItem";
+import OrderSummary from "./OrderSummary";
+import "./cart.css";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const cart = cartInfo;
+  const totalPrice = cart.reduce(
+    (acc, cur) => acc + cur.price * cur.quantity,
+    0
+  );
+  return (
+    <div className="cart">
+      <div className="cart-title">Your Shopping Cart</div>
+      <div className="cart-wrapper">
+        <div className="cart-items">
+          {cart.map((item) => (
+            <CartItem key={item.id} item={item} />
+          ))}
+        </div>
+        <OrderSummary totalPrice={totalPrice} />
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
---
+++ src/pages/cart/CartItem.jsx
@@ -0,0 +1,33 @@
+const CartItem = ({ item }) => {
+  const { image, title, author, price, quantity } = item;
+  return (
+    <div className="cart-item">
+      <img src={`/books/${image}`} alt={title} className="cart-item-img" />
+      <div className="cart-item-info">
+        <div>
+          <div className="cart-item-book-title">
+            <b>Title: </b> {title}
+          </div>
+          <div className="cart-item-author">
+            <b>Author: </b> {author}
+          </div>
+        </div>
+        <div>
+          <div className="cart-item-quantity">
+            <button>
+              <i className="bi bi-plus-lg"></i>
+            </button>
+            <b>{quantity}</b>
+            <button>
+              <i className="bi bi-dash-lg"></i>
+            </button>
+          </div>
+          <div className="cart-items-price">${price * quantity}</div>
+          <i className="bi bi-trash-fill"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
