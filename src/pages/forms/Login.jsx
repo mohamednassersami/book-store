bbcbddaf663eab65d1c16942ed703fc3b893cad6
mohamednasser <mohamed.nasser.sami.97@gmail.com>
2023-10-23 07:27:07 +0300

    handle login and register pages

--- src/pages/forms/Login.jsx
+++ src/pages/forms/Login.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./forms.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  // Form Submit Handler
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (email.trim() === "") return toast.error("Email is required");
+    if (password.trim() === "") return toast.error("Password is required");
+  };
+
+  // Show Password Handler
+  const showPasswordHandler = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  return (
+    <div onSubmit={formSubmitHandler} className="form-wrapper">
+      <ToastContainer theme="colored" />
+      <h1 className="form-title">Login to your account</h1>
+      <form className="form">
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type={showPassword ? "text" : "Password"}
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {showPassword && (
+          <i
+            className="bi bi-eye-slash-fill show-password-icon"
+            onClick={showPasswordHandler}
+          ></i>
+        )}
+        {!showPassword && (
+          <i
+            className="bi bi-eye-fill show-password-icon"
+            onClick={showPasswordHandler}
+          ></i>
+        )}
+
+        <button className="form-btn">Login</button>
+      </form>
+      <div className="form-footer">
+        Don't have an account?{" "}
+        <Link to="/register" className="form-link">
+          Register
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
