f210ee833e35bd373b3cae882f26f16915219540
mohamednasser <mohamed.nasser.sami.97@gmail.com>
2023-10-23 07:26:35 +0300

    create authors,about,contact pages

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import "./about.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="about">
+      <h1>About Us</h1>
+      <p>We are an online book store</p>
+      <p>
+        <strong>Version: 1.0.0</strong>
+      </p>
+    </section>
+  );
 };
 
 export default About;
--- src/pages/authors/Authors.jsx
+++ src/pages/authors/Authors.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useState } from "react";
+import { authors } from "../../data/authors";
+import "./authors.css";
 
 const Authors = () => {
-  return <div>Authors</div>;
+  const [search, setSearch] = useState("");
+
+  return (
+    <section className="authors">
+      <div className="authors-search-wrapper">
+        <input
+          type="search"
+          placeholder="Search in authors"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div className="authors-wrapper">
+        {authors
+          .filter((author) =>
+            author.name.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((author) => (
+            <div key={author.id} className="author">
+              <img
+                src={author.image}
+                alt={author.name}
+                className="author-img"
+              />
+              <h2 className="author-name">{author.name}</h2>
+            </div>
+          ))}
+      </div>
+    </section>
+  );
 };
 
 export default Authors;
